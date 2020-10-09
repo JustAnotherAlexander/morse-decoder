9fc1bbb7885bf95f714be503bda8f3c9bc18158d
Aliaksandr Matveyankou <aleksandreliuz@gmail.com>
2020-10-09 10:24:20 +0300

    init: start solving morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    expr=expr.split('');
+    let result =[];
+    let dots_and_dashes = [];
+
+    while (expr.length>0) {
+    result.push(expr.splice(0,10));
+    }
+    console.log('morse code: '+ result);
+    console.log('result[0]: '+result[0]);
+    for (let j=0; j<result.length; j++) {
+        for (let i=0; i<10; i+2) {
+            dots_and_dashes.push(result[j].splice(i,2));
+        }
+        for (let k=0; k<10; k++) {
+            if ((dots_and_dashes[k][0]==1 && dots_and_dashes[k][1]==1)){
+                dots_and_dashes.splice(0,2,'-');
+            } else if ((dots_and_dashes[k][0]==1 && dots_and_dashes[k][1]==0)) {
+                dots_and_dashes.splice(0,2,'.');
+              } else if ((dots_and_dashes[k][0]==0 && dots_and_dashes[k][1]==0)) {
+                dots_and_dashes.splice(k,1);
+            }
+        }
+        result[j]=dots_and_dashes.join('');
+    }
+    
+    console.log(result);
 }
+
 
 module.exports = {
     decode
